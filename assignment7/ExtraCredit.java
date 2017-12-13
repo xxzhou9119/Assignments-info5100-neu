61de4be01f922d00126e96d009b5ac993faeaba1
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-12 16:49:32 -0800

    score update

--- assignment7/ExtraCredit.java
+++ assignment7/ExtraCredit.java
@@ -1,7 +1,9 @@
-
+/* Good Work
+ * Score 9.5 + extra credit 2; Total score 10
+ */
 public class ExtraCredit {
 	
-	public static boolean findPartition(int[] arr){
+	public static boolean findPartition(int[] arr){ // extra credit 2
 		int sum=0;
 		for(int i=0;i<arr.length;i++){
 			sum+=arr[i];
