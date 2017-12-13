7c778e42dc2a3c3805899e8b1fb4c87b4f0de7d5
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-12 22:14:42 -0800

    score update

--- assignment8/LyricAnalyzer.java
+++ assignment8/LyricAnalyzer.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class LyricAnalyzer {
+public class LyricAnalyzer { // score 4
 	private HashMap<String, ArrayList<Integer>> map=new HashMap<String, ArrayList<Integer>>();
 	
 	public void read(File file) throws IOException, FileNotFoundException{
